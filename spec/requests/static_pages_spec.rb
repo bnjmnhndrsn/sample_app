18bfe36395a3d2a4d011a40f678a69d3e264809b
Ben <benjamin.c.henderson@gmail.com>
2014-09-13 15:59:18 -0700

    Make a basic User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -55,7 +55,7 @@ describe "Static pages" do
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     expect(page).to have_title(full_title(""))
-    click_link "Sign up now!"
+    click_link "Sign up now!" 
     expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
     expect(page).to have_title(full_title(""))
